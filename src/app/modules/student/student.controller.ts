62504c4f832223897b33a71fb47a2b27d6b5f6ee
Mohammad Arif <programmermdarif@gmail.com>
2024-05-30 09:45:30 +0600

    Created catch asycnce middleware

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -2,42 +2,21 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { StudentServices } from './student.service';
+import catchAsync from '../../utils/catchAsync';
 
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(studentId);
 
-
-
-const catchAsync = (fn : RequestHandler)=>{
-  return (req:Request,res:Response,next : NextFunction)=>{
-    Promise.resolve(fn(req,res,next)).catch(err=> next(err))
-  }
- 
-}
-
-
-const getSingleStudent  = catchAsync( async (
-  req,
-  res,
-  next
-) => {
-  
-    const { studentId } = req.params;
-    const result = await StudentServices.getSingleStudentFromDB(studentId);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is retrieved succesfully',
-      data: result,
-    });
-  
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is retrieved succesfully',
+    data: result,
+  });
 });
 
-const getAllStudents: RequestHandler = async (
-  req,
-  res,
-  next,
-) => {
-  try {
+const getAllStudents: RequestHandler = catchAsync(async (req, res, next) => {
     const result = await StudentServices.getAllStudentsFromDB();
 
     sendResponse(res, {
@@ -46,16 +25,9 @@ const getAllStudents: RequestHandler = async (
       message: 'Student are retrieved succesfully',
       data: result,
     });
-  } catch (err) {
-    next(err);
-  }
-};
+});
 
-const deleteStudent: RequestHandler = async (
-  req,
-  res,
-  next,
-) => {
+const deleteStudent: RequestHandler = async (req, res, next) => {
   try {
     const { studentId } = req.params;
     const result = await StudentServices.deleteStudentFromDB(studentId);

df9811e9f6185d4246404ceccddef26e7c71350f
Mohammad Arif <programmermdarif@gmail.com>
2024-06-20 19:13:12 +0600

    Added the search term feature

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -5,8 +5,21 @@ import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 import { Student } from './student.model';
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query : Record<string,unknown>) => {
+
+  let searchTerm = ''
+
+  if(query.searchTerm){
+    searchTerm = query.searchTerm as string
+  }
+
+  const result = await Student.find({
+    $or:['email','name.firstName','presentAddress'].map(
+      (field)=>({
+        [field] : {$regex : searchTerm , $options: 'i'}
+      })
+    )
+  })
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import {Server} from 'http'
+
+let server : Server;
+
 
 async function main() {
   try {
@@ -15,3 +19,21 @@ async function main() {
 }
 
 main();
+
+
+
+process.on('unhandledRejection',()=>{
+  console.log('UnhandleRejection is detected, shutting the server')
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }
+  process.exit(1)
+})
+
+
+process.on('uncaughtException',()=>{
+  console.log('UncaughtException is detected, shutting the server')
+  process.exit(1)
+})

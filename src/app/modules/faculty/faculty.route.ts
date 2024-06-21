0140a49da1ef1430b198a2c2d2bd58f823829a9a
Mohammad Arif <programmermdarif@gmail.com>
2024-06-21 12:07:51 +0600

    Added the faculty validaiton

---
+++ src/app/modules/faculty/faculty.route.ts
@@ -0,0 +1,18 @@
+import validateRequest from "../../middlewares/validateRequest";
+
+
+const router = express.Router();
+
+router.get('/:studentId', StudentControllers.getSingleStudent);
+
+router.patch(
+  '/:studentId',
+  validateRequest(updateStudentValidationSchema),
+  StudentControllers.updateStudent,
+);
+
+router.delete('/:studentId', StudentControllers.deleteStudent);
+
+router.get('/', StudentControllers.getAllStudents);
+
+export const StudentRoutes = router;

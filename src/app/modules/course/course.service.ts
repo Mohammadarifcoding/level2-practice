f06c57038a32ba2414aa90f558e13639695baa3d
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 17:16:26 +0600

    Added course model and interface

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,28 @@
+// import { Course } from './course.model';
+
+// const createCourseIntoDb = async () => {
+//   const result = await Course.create();
+//   return result;
+// };
+
+// const getAllCoursesFromDb = async () => {
+//   const result = await Course.find();
+//   return result;
+// };
+
+// const getSingleCourseFromDb = async (id) => {
+//   const result = await Course.findById(id);
+//   return result;
+// };
+
+// const updateCourseFromDb = async (id: string, payload) => {
+//   const result = await Course.findByIdAndUpdate(id, payload);
+//   return result;
+// };
+
+// export const CourseServices = {
+//   createCourseIntoDb,
+//   getAllCoursesFromDb,
+//   getSingleCourseFromDb,
+//   updateCourseFromDb,
+// };

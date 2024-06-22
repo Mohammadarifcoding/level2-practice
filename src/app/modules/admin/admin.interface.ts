7d12f30135da044c820f2acc77dc50b5292a9dce
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 11:15:12 +0600

    Added the admin model an interface

---
+++ src/app/modules/admin/admin.interface.ts
@@ -0,0 +1,36 @@
+import {  Types } from 'mongoose';
+
+export type TGender = 'male' | 'female' | 'other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TAdmin = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TUserName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  isDeleted: boolean;
+};
+

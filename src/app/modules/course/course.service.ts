f1ddcaba6a1df54e8b7db7eded1f5fe80f7307b6
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 17:16:59 +0600

    Added course services for curd operation

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,28 +1,28 @@
-// import { Course } from './course.model';
+import { Course } from './course.model';
 
-// const createCourseIntoDb = async () => {
-//   const result = await Course.create();
-//   return result;
-// };
+const createCourseIntoDb = async () => {
+  const result = await Course.create();
+  return result;
+};
 
-// const getAllCoursesFromDb = async () => {
-//   const result = await Course.find();
-//   return result;
-// };
+const getAllCoursesFromDb = async () => {
+  const result = await Course.find();
+  return result;
+};
 
-// const getSingleCourseFromDb = async (id) => {
-//   const result = await Course.findById(id);
-//   return result;
-// };
+const getSingleCourseFromDb = async (id) => {
+  const result = await Course.findById(id);
+  return result;
+};
 
-// const updateCourseFromDb = async (id: string, payload) => {
-//   const result = await Course.findByIdAndUpdate(id, payload);
-//   return result;
-// };
+const updateCourseFromDb = async (id: string, payload) => {
+  const result = await Course.findByIdAndUpdate(id, payload);
+  return result;
+};
 
-// export const CourseServices = {
-//   createCourseIntoDb,
-//   getAllCoursesFromDb,
-//   getSingleCourseFromDb,
-//   updateCourseFromDb,
-// };
+export const CourseServices = {
+  createCourseIntoDb,
+  getAllCoursesFromDb,
+  getSingleCourseFromDb,
+  updateCourseFromDb,
+};

1f300271e14f90481a52d951d27a52dfd54f3931
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 18:16:40 +0600

    Added course route:

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,12 +1,13 @@
+import { TCourse } from './course.interface';
 import { Course } from './course.model';
 
-const createCourseIntoDb = async () => {
-  const result = await Course.create();
+const createCourseIntoDb = async (payload:TCourse) => {
+  const result = await Course.create(payload);
   return result;
 };
 
-const getAllCoursesFromDb = async () => {
-  const result = await Course.find();
+const getAllCoursesFromDb = async (query:any) => {
+  const result = await Course.find(query);
   return result;
 };
 
@@ -20,9 +21,14 @@ const updateCourseFromDb = async (id: string, payload) => {
   return result;
 };
 
+const deleteCourseFromDb = async(id)=>{
+    const result = await Course.findByIdAndUpdate(id,{isDeleted:true})
+}
+
+
 export const CourseServices = {
   createCourseIntoDb,
   getAllCoursesFromDb,
   getSingleCourseFromDb,
-  updateCourseFromDb,
+  updateCourseFromDb,deleteCourseFromDb
 };
